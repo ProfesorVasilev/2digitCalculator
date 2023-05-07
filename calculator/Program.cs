3a7bb5a23643fb23c63e727a26f92da36f6bc60b
ProfesorVasilev <vladigotaniq@gmail.com>
2023-05-07 11:29:49 +0300

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter number A:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter number B:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("What would you like to do with those number?");
+char action = char.Parse(Console.ReadLine());
+if (action == '+')
+{ 
+    double result = a + b;
+    Console.WriteLine(result);
+}
+else if (action == '-')
+{ 
+    double result = a - b;
+    Console.WriteLine(result);
+}
+else if (action == '*')
+{
+    double result = a * b;
+    Console.WriteLine(result);
+}
+else if (action == '/')
+{
+    double result = a / b;
+    Console.WriteLine(result);
+}
+else if (action == '%')
+{
+    double result = a % b;
+    Console.WriteLine(result);
+}
